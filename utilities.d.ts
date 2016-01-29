8fc3ce806d13fd9a56741684414a02722bc17c13
devel-pa <devel.pa@gmail.com>
2016-01-29 18:41:35 +0100

    Fix module name.

--- utilities.d.ts
+++ utilities.d.ts
@@ -1,10 +1,10 @@
 
 declare module "utilities" {
-    import * as tmp from 'utilitiesjs/lib/index';
+    import * as tmp from 'utilities/lib/index';
 	export = tmp;
 }
 
-declare module "utilitiesjs/lib/index" {
-    import * as tmp from 'lib/index';
-	export = tmp;
-}
+// declare module "utilitiesjs/lib/index" {
+//     import * as tmp from 'lib/index';
+// 	export = tmp;
+// }
